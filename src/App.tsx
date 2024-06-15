4459fc1273709b829d48fb447cf2d9cd385f1369
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-15 16:23:00 +0400

    Add: Public Layout For Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import { PublicLayout } from "./layout";
+
 import "./index.css";
 
+const HomePage = lazy(() => import("./pages/HomePage"));
+
 function App() {
-  return <p className="text-3xl font-bold underline">asa</p>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
---
+++ src/layout/PublicLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
+
+export function PublicLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+export default PublicLayout;
