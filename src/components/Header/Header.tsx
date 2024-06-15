c38310317a000bb4c74f74b82a02042a997c514c
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-15 16:37:27 +0400

    Add: Header HTML

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,50 @@
 export function Header() {
-  return <div>header</div>;
+  return (
+    <header>
+      <section>
+        <img src="" alt="Agrios Logo" />
+        <div>
+          <ul>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li>
+              <img src="" alt="" />
+            </li>
+          </ul>
+          <address>
+            <div>
+              <img src="" alt="Phone Icon" />
+              <div>
+                <p>Call anytime</p>
+                <p>+ 98 (000) - 9630</p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="Email Icon" />
+              <div>
+                <p>Send email</p>
+                <p>TornikeTsagareishvili64@gmail.com</p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="Location Icon" />
+              <div>
+                <p>380 St Kilda Road</p>
+                <p>Melbourne, Australia</p>
+              </div>
+            </div>
+          </address>
+        </div>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
