77907943d3bf4e6b63479f39d8b0180d8af3ba33
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-16 21:48:39 +0400

    Add: Header Style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -39,15 +39,17 @@ export function Header() {
   ];
 
   return (
-    <header className="w-full bg-#ffffff flex items-center justify-center">
+    <header className="w-full bg-#ffffff h-[115px] flex items-center justify-center">
       <section className="w-[75%] max-w-[1200px] flex  items-center justify-between">
         <img src={agriosLogo} alt="Agrios Logo" />
         <div className="flex">
-          {socialMedia.map((logo) => {
+          {socialMedia.map((logo, index) => {
             return (
               <ul
                 key={uuidv4()}
-                className="flex items-center list-none cursor-pointer mr-2.5"
+                className={`flex items-center list-none cursor-pointer ${
+                  index === contactInfo.length ? "mr-0" : "mr-2.5"
+                }`}
               >
                 <li className="bg-[#f0f0f0] flex p-[7px] rounded-[100%] hover:bg-[#D1D1D1]">
                   <img src={logo.src} alt={logo.alt} />
@@ -55,14 +57,23 @@ export function Header() {
               </ul>
             );
           })}
-          <address className="flex items-center">
-            {contactInfo.map((info) => {
+          <address className="flex items-center not-italic">
+            {contactInfo.map((info, index) => {
               return (
-                <div key={uuidv4()} className="flex">
-                  <img src={info.src} alt={info.alt} />
+                <div
+                  key={uuidv4()}
+                  className={`flex items-center justify-center px-5 py-2.5 ${
+                    index === 1
+                      ? "border-x-2 border-solid border-[#E4E2D7]"
+                      : ""
+                  }`}
+                >
+                  <img className="p-2.5" src={info.src} alt={info.alt} />
                   <div>
-                    <p>{info.text}</p>
-                    <p>{info.additonalText}</p>
+                    <p className="text-[#878680] text-[12px]">{info.text}</p>
+                    <p className="whitespace-nowrap text-[14px] font-bold">
+                      {info.additonalText}
+                    </p>
                   </div>
                 </div>
               );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("not-last", "&:not(:last-child)");
+    }),
+  ],
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
 };
