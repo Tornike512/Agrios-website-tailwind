e0073163c25fdd18521f2f76ce35a484fec984e2
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-20 23:07:50 +0400

    Start: Offer Section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import { HeaderSection } from "./HeaderSection";
-import { FeaturesSection } from "../FeaturesSection";
+import { FeaturesSection } from "./FeaturesSection";
+import { OfferSection } from "./OfferSection";
 
 export function HomePage() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <HeaderSection />
       <FeaturesSection />
+      <OfferSection />
     </div>
   );
 }
---
+++ src/pages/HomePage/OfferSection/OfferSection.tsx
@@ -0,0 +1,5 @@
+export function OfferSection() {
+  return <section></section>;
+}
+
+export default OfferSection;
