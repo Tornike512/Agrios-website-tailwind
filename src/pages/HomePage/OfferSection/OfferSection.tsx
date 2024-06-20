2e1ac18f531974d8d1e0e6a6b46078ca1308244b
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-20 23:19:42 +0400

    Add: OfferSection HTML

--- src/pages/HomePage/OfferSection/OfferSection.tsx
+++ src/pages/HomePage/OfferSection/OfferSection.tsx
@@ -1,5 +1,51 @@
 export function OfferSection() {
-  return <section></section>;
+  return (
+    <section>
+      <div></div>
+      <p>Our Services</p>
+      <h2>What We Offer</h2>
+      <ul>
+        <li>
+          <img src="" alt="Apple Image" />
+          <img src="" alt="Tractor Icon" />
+          <button>Read More</button>
+          <div>
+            <p>Agriculture Products</p>
+          </div>
+        </li>
+        <li>
+          <img src="" alt="Vegetables Image" />
+          <img src="" alt="Vegetable Icon" />
+          <button>Read More</button>
+          <div>
+            <p>Organic Products</p>
+          </div>
+        </li>
+        <li>
+          <img src="" alt="Plant Image" />
+          <img src="" alt="Plant Icon" />
+          <button>Read More</button>
+          <div>
+            <p>Fresh Vegetables</p>
+          </div>
+        </li>
+        <li>
+          <img src="" alt="Grapes Image" />
+          <img src="" alt="Milk Icon" />
+          <button>Read More</button>
+          <div>
+            <p>Dairy Products</p>
+          </div>
+        </li>
+      </ul>
+      <div>
+        <p>Agriculture Products</p>
+        <p>Projects Completed</p>
+        <p>Satisfied Clients</p>
+        <p>Experts Farmers</p>
+      </div>
+    </section>
+  );
 }
 
 export default OfferSection;
