f097983bcf737339e9b2b80c5be0bea1969560ef
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-20 23:33:21 +0400

    Document: Add Images To OfferSection

--- src/pages/HomePage/OfferSection/OfferSection.tsx
+++ src/pages/HomePage/OfferSection/OfferSection.tsx
@@ -1,37 +1,46 @@
+import applesImage from "src/assets/apples-image.svg";
+import grapesImage from "src/assets/grapes-image.svg";
+import milkIcon from "src/assets/milk-icon.svg";
+import plantIcon from "src/assets/plant-icon-2.svg";
+import plantImage from "src/assets/plant-image.svg";
+import tractorIcon from "src/assets/tractor-icon.svg";
+import vegetablesImage from "src/assets/vegetables-image.svg";
+import vegetablesIcon from "src/assets/vegetables-icon.svg";
+
 export function OfferSection() {
   return (
-    <section>
-      <div></div>
+    <section className="max-w-[1200px] relative w-full ">
+      <div className="absolute z-[-2] top-0 w-[1200px] h-[400px] bg-[#F8F7F0]"></div>
       <p>Our Services</p>
       <h2>What We Offer</h2>
       <ul>
         <li>
-          <img src="" alt="Apple Image" />
-          <img src="" alt="Tractor Icon" />
+          <img src={applesImage} alt="Apple Image" />
+          <img src={tractorIcon} alt="Tractor Icon" />
           <button>Read More</button>
           <div>
             <p>Agriculture Products</p>
           </div>
         </li>
         <li>
-          <img src="" alt="Vegetables Image" />
-          <img src="" alt="Vegetable Icon" />
+          <img src={vegetablesImage} alt="Vegetables Image" />
+          <img src={vegetablesIcon} alt="Vegetable Icon" />
           <button>Read More</button>
           <div>
             <p>Organic Products</p>
           </div>
         </li>
         <li>
-          <img src="" alt="Plant Image" />
-          <img src="" alt="Plant Icon" />
+          <img src={plantImage} alt="Plant Image" />
+          <img src={plantIcon} alt="Plant Icon" />
           <button>Read More</button>
           <div>
             <p>Fresh Vegetables</p>
           </div>
         </li>
         <li>
-          <img src="" alt="Grapes Image" />
-          <img src="" alt="Milk Icon" />
+          <img src={grapesImage} alt="Grapes Image" />
+          <img src={milkIcon} alt="Milk Icon" />
           <button>Read More</button>
           <div>
             <p>Dairy Products</p>
