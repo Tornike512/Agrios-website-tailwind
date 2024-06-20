a6623290125d40634058e9c21d8752a606c7dcb1
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-20 19:13:02 +0400

    Add: Header Section HTML

--- src/pages/HomePage/HeaderSection/HeaderSection.tsx
+++ src/pages/HomePage/HeaderSection/HeaderSection.tsx
@@ -1,5 +1,17 @@
 export function HeaderSection() {
-  return <section></section>;
+  return (
+    <section>
+      <img src="" alt="Header Section Main Image" />
+      <p>Welcome to agrios farming</p>
+      <h1>Agriculture & Eco Farming</h1>
+      <img src="" alt="Sign Icon" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+        luctus nec ullamcorper mattis, pulvinar dapibus leo.
+      </p>
+      <button>Discover More</button>
+    </section>
+  );
 }
 
 export default HeaderSection;
