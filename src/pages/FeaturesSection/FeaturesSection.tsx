2811ba1d6858f9ee0e0f3a4c5e04ae45d82e16a8
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-20 21:09:30 +0400

    Add: FeaturesSection HTML

--- src/pages/FeaturesSection/FeaturesSection.tsx
+++ src/pages/FeaturesSection/FeaturesSection.tsx
@@ -1,5 +1,60 @@
 export function FeaturesSection() {
-  return <section>asgas</section>;
+  return (
+    <section className="w-[1200px]">
+      <ul>
+        <li>
+          <p>Feature 01</p>
+          <h3>We're using a new technology</h3>
+          <img src="" alt="Technology Image" />
+        </li>
+        <li>
+          <p>Feature 02</p>
+          <h3>Good in smart organic services</h3>
+          <img src="" alt="Organic Service Image" />
+        </li>
+        <li>
+          <p>Feature 03</p>
+          <h3>Reforming in the systems</h3>
+          <img src="" alt="Reforming Image" />
+        </li>
+      </ul>
+      <div>
+        <section>
+          <img src="" alt="Tractor Image" />
+          <img src="" alt="Plant Image" />
+        </section>
+        <article>
+          <p>Our Introductions</p>
+          <h2>Agriculture & Organic Product Farm</h2>
+          <p>Agrios is the largest global organic farm.</p>
+          <p>
+            There are many variations of passages of lorem ipsum available but
+            the majority have suffered alteration in some form by injected humor
+            or random word which don’t look even.
+          </p>
+          <ul>
+            <li>
+              <img src="" alt="Fruits image" />
+              <h4>Growing fruits vegetables</h4>
+            </li>
+            <li>
+              <img src="" alt="Ripening Image" />
+              <h4>Tips for ripening your fruits</h4>
+            </li>
+          </ul>
+          <p>
+            <img src="" alt="Confirm Image" />
+            Lorem Ipsum is not simply random text.
+          </p>
+          <p>
+            <img src="" alt="Confirm Image" />
+            Making this the first true generator on the internet.
+          </p>
+          <button>Discover More</button>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default FeaturesSection;
