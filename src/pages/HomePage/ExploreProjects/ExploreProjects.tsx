152b6d76b8fd7c8597810193bc182293e18298e7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-22 01:13:30 +0400

    Add: Explore Projects Section HTML

--- src/pages/HomePage/ExploreProjects/ExploreProjects.tsx
+++ src/pages/HomePage/ExploreProjects/ExploreProjects.tsx
@@ -1,5 +1,28 @@
 export function ExploreProjects() {
-  return <section></section>;
+  return (
+    <section>
+      <p>Recently Completed</p>
+      <h2>Explore Projects</h2>
+      <div>
+        <figure>
+          <img src="" alt="Wheat Image" />
+          <figcaption>Easy Harvesting</figcaption>
+        </figure>
+        <figure>
+          <img src="" alt="Tractor Image" />
+          <figcaption>Agriculture Farming</figcaption>
+        </figure>
+        <figure>
+          <img src="" alt="Wheat Image" />
+          <figcaption>Ecological Farming</figcaption>
+        </figure>
+        <figure>
+          <img src="" alt="Wheat Farming Image" />
+          <figcaption>Organic Solutions</figcaption>
+        </figure>
+      </div>
+    </section>
+  );
 }
 
 export default ExploreProjects;
