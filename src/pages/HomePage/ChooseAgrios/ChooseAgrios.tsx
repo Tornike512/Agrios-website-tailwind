b3cb5f18977ee1fd20d17f7788cbf63fc827d0a4
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 17:02:39 +0400

    Add: Choose Agrios Section HTML

--- src/pages/HomePage/ChooseAgrios/ChooseAgrios.tsx
+++ src/pages/HomePage/ChooseAgrios/ChooseAgrios.tsx
@@ -1,5 +1,51 @@
 export function ChooseAgrios() {
-  return <section></section>;
+  return (
+    <section>
+      <img src="" alt="Choose Agrios Section Main Image" />
+      <figure>
+        <p>Our Farm Benefits</p>
+        <h2>Why Choose Agrios Market</h2>
+        <p>
+          There are many variations of passages available, but the majority have
+          suffered alteration in some form by injected humor or random words
+          which don't look even.
+        </p>
+        <ul>
+          <li>
+            <img src="" alt="Green Correct Icon" />
+            <div>
+              <h3>Quality Organic Food</h3>
+              <p>
+                There are variations. You need to be sure there is nothing
+                hidden in the middle of the text.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="Light Green Correct Icon" />
+            <div>
+              <h3>Professional Farmers</h3>
+              <p>
+                There are variations. You need to be sure there is nothing
+                hidden in the middle of the text.
+              </p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="Yellow Correct Icon" />
+            <div>
+              <h3>Quality Products</h3>
+              <p>
+                There are variations. You need to be sure there is nothing
+                hidden in the middle of the text.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </figure>
+      <button>Discover More</button>
+    </section>
+  );
 }
 
 export default ChooseAgrios;
