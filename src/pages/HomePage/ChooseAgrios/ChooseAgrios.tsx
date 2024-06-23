71619a0edba8f547d91bbea603928c52146aebf8
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 17:25:59 +0400

    Add: Choose Agrios Style

--- src/pages/HomePage/ChooseAgrios/ChooseAgrios.tsx
+++ src/pages/HomePage/ChooseAgrios/ChooseAgrios.tsx
@@ -5,50 +5,76 @@ import yellowCorrectIcon from "src/assets/yellow-correct-icon.svg";
 
 export function ChooseAgrios() {
   return (
-    <section>
-      <img src={chooseAgriosImage} alt="Choose Agrios Section Main Image" />
+    <section className="my-[120px] w-full max-w-[1200px] flex">
+      <img
+        className="mr-[10px]"
+        src={chooseAgriosImage}
+        alt="Choose Agrios Section Main Image"
+      />
       <figure>
-        <p>Our Farm Benefits</p>
-        <h2>Why Choose Agrios Market</h2>
-        <p>
+        <p className="text-[24px] text-[#EEC044]">Our Farm Benefits</p>
+        <h2 className="text-[48px] text-[#1F1E17] font-bold leading-[55px] mb-[30px]">
+          Why Choose Agrios Market
+        </h2>
+        <p className="text-[16px] text-[#878680] mb-[34px]">
           There are many variations of passages available, but the majority have
           suffered alteration in some form by injected humor or random words
           which don't look even.
         </p>
         <ul>
-          <li>
-            <img src={greenCorrectIcon} alt="Green Correct Icon" />
+          <li className="flex items-start">
+            <img
+              className="mr-[25px] mt-[7px] items-start"
+              src={greenCorrectIcon}
+              alt="Green Correct Icon"
+            />
             <div>
-              <h3>Quality Organic Food</h3>
-              <p>
+              <h3 className="text-[24px] text-[#1F1E17] font-bold">
+                Quality Organic Food
+              </h3>
+              <p className="text-[16px] text-[#878680]">
                 There are variations. You need to be sure there is nothing
                 hidden in the middle of the text.
               </p>
             </div>
           </li>
-          <li>
-            <img src={lightGreenCorrectIcon} alt="Light Green Correct Icon" />
+          <li className="flex items-start mt-[23px]">
+            <img
+              className="mr-[25px] mt-[7px] items-start"
+              src={lightGreenCorrectIcon}
+              alt="Light Green Correct Icon"
+            />
             <div>
-              <h3>Professional Farmers</h3>
-              <p>
+              <h3 className="text-[24px] text-[#1F1E17] font-bold">
+                Professional Farmers
+              </h3>
+              <p className="text-[16px] text-[#878680]">
                 There are variations. You need to be sure there is nothing
                 hidden in the middle of the text.
               </p>
             </div>
           </li>
-          <li>
-            <img src={yellowCorrectIcon} alt="Yellow Correct Icon" />
+          <li className="flex items-start mt-[23px]">
+            <img
+              className="mr-[25px] mt-[7px] items-start"
+              src={yellowCorrectIcon}
+              alt="Yellow Correct Icon"
+            />
             <div>
-              <h3>Quality Products</h3>
-              <p>
+              <h3 className="text-[24px] text-[#1F1E17] font-bold">
+                Quality Products
+              </h3>
+              <p className="text-[16px] text-[#878680]">
                 There are variations. You need to be sure there is nothing
                 hidden in the middle of the text.
               </p>
             </div>
           </li>
         </ul>
+        <button className="mt-[50px] bg-[#4BAF47] font-bold w-fit px-[50px] py-4 rounded-[10px] hover:bg-[#54c950] text-[#ffffff]">
+          Discover More
+        </button>
       </figure>
-      <button>Discover More</button>
     </section>
   );
 }
