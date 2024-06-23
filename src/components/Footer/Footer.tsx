0fd4efd66c1dac993665919dd191dd96f3d9b98f
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-24 00:48:22 +0400

    Add: Footer Style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,8 +15,8 @@ export function Footer() {
   return (
     <>
       <footer className="bg-[#24231D] flex justify-center py-[120px]">
-        <div className="flex max-w-[1200px] w-full text-[#A5A49A]">
-          <ul className="flex">
+        <div className="flex max-w-[1200px] w-full text-[#A5A49A] justify-between">
+          <ul className="flex mr-[65px]">
             <li className="max-w-[270px] w-full px-[10px]">
               <img
                 className="mb-[21px]"
@@ -50,69 +50,94 @@ export function Footer() {
                 />
               </figure>
             </li>
-            <li>
-              <h3>Explore</h3>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+            <li className="mr-[35px]">
+              <h3 className="text-[#ffffff] text-[20px] font-bold max-w-[74px] w-full border-b-[5px] border-b-[#4BAF47] border-solid mb-[20px]">
+                Explore
+              </h3>
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px] " src={leafIcon} alt="Leaf Icon" />
                 <figcaption>About</figcaption>
               </figure>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px]" src={leafIcon} alt="Leaf Icon" />
                 <figcaption>Services</figcaption>
               </figure>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px]" src={leafIcon} alt="Leaf Icon" />
                 <figcaption>Our Projects</figcaption>
               </figure>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px]" src={leafIcon} alt="Leaf Icon" />
                 <figcaption>Meet the Farmers</figcaption>
               </figure>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px]" src={leafIcon} alt="Leaf Icon" />
                 <figcaption>Latest News</figcaption>
               </figure>
-              <figure>
-                <img src={leafIcon} alt="Leaf Icon" />
+              <figure className="flex mb-[6px]">
+                <img className="mr-[10px]" src={leafIcon} alt="Leaf Icon" />
                 <figcaption>Contact</figcaption>
               </figure>
             </li>
-            <li>
-              <h3>News</h3>
-              <div>
-                <h4>Bringing Food Production Back To Cities</h4>
-                <time>July 5, 2022</time>
+            <li className="w-full max-w-[190px]">
+              <h3 className="text-[#ffffff] text-[20px] font-bold max-w-[74px] w-full border-b-[5px] border-b-[#4BAF47] border-solid mb-[20px]">
+                News
+              </h3>
+              <div className="mb-[32px]">
+                <h4 className="text-[#ffffff]">
+                  Bringing Food Production Back To Cities
+                </h4>
+                <time className="text-[#EEC044]">July 5, 2022</time>
               </div>
               <div>
-                <h4>The Future of Farming, Smart Irrigation Solutions</h4>
-                <time>July 5, 2022</time>
+                <h4 className="text-[#ffffff]">
+                  The Future of Farming, Smart Irrigation Solutions
+                </h4>
+                <time className="text-[#EEC044]">July 5, 2022</time>
               </div>
             </li>
           </ul>
-          <address>
-            <h3>Contact</h3>
-            <figure>
-              <img src={yellowPhoneIcon} alt="Yellow Phone Icon" />
+          <address className="not-italic max-w-[270px] w-full px-[15px] relative">
+            <h3 className="text-[#ffffff] text-[20px] font-bold max-w-[74px] w-full border-b-[5px] border-b-[#4BAF47] border-solid mb-[20px]">
+              Contact
+            </h3>
+            <figure className="flex mb-[10px]">
+              <img
+                className="mr-[8px]"
+                src={yellowPhoneIcon}
+                alt="Yellow Phone Icon"
+              />
               <figcaption>666 888 0000</figcaption>
             </figure>
-            <figure>
-              <img src={yellowConvertIcon} alt="Yellow Convert Icon" />
+            <figure className="flex mb-[10px]">
+              <img
+                className="mr-[8px]"
+                src={yellowConvertIcon}
+                alt="Yellow Convert Icon"
+              />
               <a>Torniketsagareishvili64@gmail.com</a>
             </figure>
-            <figure>
-              <img src={yellowLocationIcon} alt="Yellow Location Icon" />
+            <figure className="flex mb-[18px]">
+              <img
+                className="mr-[8px]"
+                src={yellowLocationIcon}
+                alt="Yellow Location Icon"
+              />
               <figcaption>
                 80 broklyn golden street line New York, USA
               </figcaption>
             </figure>
-            <input type="email" />
-            <button>
+            <input
+              className="p-[10px] rounded-[10px]"
+              placeholder="Your Email Address"
+              type="email"
+            />
+            <button className="bg-[#4BAF47] p-[16px] absolute rounded-tr-[10px] rounded-br-[10px] hover:bg-[#54c950] right-[5px]">
               <img src={sendIcon} alt="Send Icon" />
             </button>
           </address>
         </div>
       </footer>
-      ;
       <FooterNavigation />
     </>
   );
--- src/components/Footer/FooterNavigation/FooterNavigation.tsx
+++ src/components/Footer/FooterNavigation/FooterNavigation.tsx
@@ -1,5 +1,12 @@
 export function FooterNavigation() {
-  return <div></div>;
+  return (
+    <section className="bg-[#1F1E17] flex justify-center h-[70px]">
+      <div className="text-[#A5A49A] flex justify-between items-center w-full max-w-[1200px]">
+        <p>© All Copyright 2024 by shawonetc Themes</p>
+        <p>Terms of Use | Privacy Policy</p>
+      </div>
+    </section>
+  );
 }
 
 export default FooterNavigation;
