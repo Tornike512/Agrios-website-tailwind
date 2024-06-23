dd7a3bebb353b1f2e738da7f93c93d26b3e5d598
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 18:20:18 +0400

    Document: Import All Images On NewsSection

--- src/pages/HomePage/NewsSection/NewsSection.tsx
+++ src/pages/HomePage/NewsSection/NewsSection.tsx
@@ -1,12 +1,15 @@
+import agronomyImage from "src/assets/agronomy-image.svg";
+import foodProductionImage from "src/assets/food-production-image.svg";
+
 export function NewsSection() {
   return (
     <article>
       <h4>From the Blog</h4>
       <h2>News & Articles</h2>
       <figure className="">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={foodProductionImage} alt="Wheat Farming Image" />
+        <img src={foodProductionImage} alt="Wheat Farming Image" />
+        <img src={agronomyImage} alt="Agronomy Image" />
       </figure>
     </article>
   );
