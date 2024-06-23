0c97f0cca57e791c05e579e4c8f26194709987d1
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 18:16:48 +0400

    Drop: Ul from NewSection

--- src/pages/HomePage/NewsSection/NewsSection.tsx
+++ src/pages/HomePage/NewsSection/NewsSection.tsx
@@ -3,53 +3,11 @@ export function NewsSection() {
     <article>
       <h4>From the Blog</h4>
       <h2>News & Articles</h2>
-      <ul>
-        <li>
-          <img src="" alt="Wheat Article Image" />
-          <figure>
-            <figcaption>
-              <img src="" alt="User Icon" />
-              <p>Kevin Martin</p>
-            </figcaption>
-            <figcaption>
-              <img src="" alt="Comment Icon" />
-              <p>1 Comment</p>
-            </figcaption>
-          </figure>
-          <h3>Bringing Food Production Back To Cities</h3>
-          <time>05 July 2022</time>
-        </li>
-        <li>
-          <img src="" alt="Wheat Article Image" />
-          <figure>
-            <figcaption>
-              <img src="" alt="User Icon" />
-              <p>Kevin Martin</p>
-            </figcaption>
-            <figcaption>
-              <img src="" alt="Comment Icon" />
-              <p>0 Comments</p>
-            </figcaption>
-          </figure>
-          <h3>The Future of Farming, Smart Irrigation Solutions</h3>
-          <time>05 July 2022</time>
-        </li>
-        <li>
-          <img src="" alt="Tractor Article Image" />
-          <figure>
-            <figcaption>
-              <img src="" alt="User Icon" />
-              <p>Kevin Martin</p>
-            </figcaption>
-            <figcaption>
-              <img src="" alt="Comment Icon" />
-              <p>0 Comments</p>
-            </figcaption>
-          </figure>
-          <h3>Agronomy and relation to Other Sciences</h3>
-          <time>05 July 2022</time>
-        </li>
-      </ul>
+      <figure className="">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </figure>
     </article>
   );
 }
