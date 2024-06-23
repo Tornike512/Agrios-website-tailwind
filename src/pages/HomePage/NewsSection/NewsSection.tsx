0f00186325906b9c834b3f83df999c6b5bd274ad
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 18:30:34 +0400

    News Section Style

--- src/pages/HomePage/NewsSection/NewsSection.tsx
+++ src/pages/HomePage/NewsSection/NewsSection.tsx
@@ -3,13 +3,27 @@ import foodProductionImage from "src/assets/food-production-image.svg";
 
 export function NewsSection() {
   return (
-    <article>
-      <h4>From the Blog</h4>
-      <h2>News & Articles</h2>
-      <figure className="">
-        <img src={foodProductionImage} alt="Wheat Farming Image" />
-        <img src={foodProductionImage} alt="Wheat Farming Image" />
-        <img src={agronomyImage} alt="Agronomy Image" />
+    <article className="flex flex-col items-center justify-center w-full max-w-[1200px] mb-[113px]">
+      <h4 className="text-[24px] text-[#EEC044]">From the Blog</h4>
+      <h2 className="text-[48px] text-[#1F1E17] font-bold leading-[55px] mb-[50px]">
+        News & Articles
+      </h2>
+      <figure className="grid grid-cols-[1fr_1fr_1fr]">
+        <img
+          className="cursor-pointer w-full"
+          src={foodProductionImage}
+          alt="Wheat Farming Image"
+        />
+        <img
+          className="ml-[8px] cursor-pointer w-full"
+          src={foodProductionImage}
+          alt="Wheat Farming Image"
+        />
+        <img
+          className="cursor-pointer w-full"
+          src={agronomyImage}
+          alt="Agronomy Image"
+        />
       </figure>
     </article>
   );
