b703771f92274559b66ff33ce3fc6127b14da1b0
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-23 16:47:21 +0400

    Add: Reviews Section STYLE

--- src/pages/HomePage/ReviewsSection/ReviewsSection.tsx
+++ src/pages/HomePage/ReviewsSection/ReviewsSection.tsx
@@ -5,34 +5,51 @@ import greenCircle from "src/assets/green-circle-image.svg";
 
 export function ReviewsSection() {
   return (
-    <section>
-      <div>
-        <section>
-          <p>Our Testimonials</p>
-          <h2>What They're Saying About Agrios</h2>
-          <p>
+    <section className="w-full flex items-center justify-center bg-[#F8F7F0] py-[120px]">
+      <div className="flex justify-center max-w-[1200px] w-full">
+        <section className="max-w-[308px] w-full mr-[30px]">
+          <p className="text-[24px] text-[#EEC044]">Our Testimonials</p>
+          <h2 className="text-[48px] text-[#1F1E17] font-bold leading-[55px] mb-[30px]">
+            What They're Saying About Agrios
+          </h2>
+          <p className="text-[16px] text-[#878680] mb-[43px]">
             There are many variations of passages available, but the majority
             have suffered alteration in some form by injected humor or random
             words which don't look even.
           </p>
-          <button>View All Testimonials</button>
+          <button className="bg-[#4BAF47] font-bold w-fit px-[50px] py-4 rounded-[10px] hover:bg-[#54c950] text-[#ffffff]">
+            View All Testimonials
+          </button>
         </section>
-        <article>
-          <div>
-            <p>
+        <article className="h-[250px] flex items-center justify-center bg-[#ffffff] rounded-[10px] mt-[10px]">
+          <div className="p-[30px]">
+            <p className="text-[18px] text-[#878680] mb-[25px]">
               There are many variations of passages available, but the majority
               have suffered alteration in some form by injected humor or random
               words which don't look even.
             </p>
-            <figure>
-              <figcaption>Bonnie Tolbert</figcaption>
-              <img src={fiveStarsImage} alt="Five Stars Image" />
+            <figure className="flex justify-between">
+              <figcaption className="text-[24px] font-bold">
+                Tornike Tsagareishvili
+              </figcaption>
+              <img
+                className=" w-full max-w-[180px]"
+                src={fiveStarsImage}
+                alt="Five Stars Image"
+              />
             </figure>
           </div>
-          <figure>
-            <img src={frameImage} alt="Frame Image" />
-            <img src={personImage} alt="Person Image" />
-            <img src={greenCircle} alt="Green Circle Image" />
+          <figure className="max-w-[450px] w-full flex items-center justify-center flex-col relative">
+            <img
+              className="w-full max-w-[150px] rounded-[100%] border-[5px] border-dotted border-[#EEC044]"
+              src={personImage}
+              alt="Person Image"
+            />
+            <img
+              className="absolute bottom-[-15px] w-[53px] left-[44%]"
+              src={greenCircle}
+              alt="Green Circle Image"
+            />
           </figure>
         </article>
       </div>
