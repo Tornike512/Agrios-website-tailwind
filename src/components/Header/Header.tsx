a1fe089c92713ab2632825919c6c68c74810b058
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-25 23:23:07 +0400

    Add: Media Query For Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ import locationIcon from "/src/assets/location-icon.svg";
 import phoneIcon from "/src/assets/phone-icon.svg";
 import pinterestLogo from "/src/assets/pinterest-logo.svg";
 import twitterLogo from "/src/assets/twitter-logo.svg";
+import menuIcon from "/src/assets/menu-icon.svg";
 
 export function Header() {
   const socialMedia = [
@@ -43,17 +44,26 @@ export function Header() {
     <>
       <header className="w-full bg-#ffffff h-[115px] flex items-center justify-center">
         <section className="max-w-[1200px] w-full flex  items-center justify-between">
-          <img className="p-[20px]" src={agriosLogo} alt="Agrios Logo" />
-          <div className="flex">
+          <img
+            className="p-[20px] lg:pr-0 lg:w-[140px] lg:pl-[10px]"
+            src={agriosLogo}
+            alt="Agrios Logo"
+          />
+          <img
+            className="hidden md:block md:w-[65px] md:px-[20px] cursor-pointer"
+            src={menuIcon}
+            alt="Menu Icon"
+          />
+          <div className="flex md:hidden">
             {socialMedia.map((logo, index) => {
               return (
                 <ul
                   key={uuidv4()}
-                  className={`flex items-center list-none cursor-pointer ${
+                  className={`flex items-center list-none cursor-pointer lg:hidden ${
                     index === contactInfo.length ? "mr-0" : "mr-2.5"
                   }`}
                 >
-                  <li className="bg-[#f0f0f0] flex p-[7px] rounded-[100%] hover:bg-[#D1D1D1]">
+                  <li className="bg-[#f0f0f0] flex p-[7px] rounded-[100%] hover:bg-[#D1D1D1]  ">
                     <img
                       className="appearance-none"
                       src={logo.src}
@@ -68,16 +78,22 @@ export function Header() {
                 return (
                   <div
                     key={uuidv4()}
-                    className={`flex items-center justify-center px-5 py-2.5 ${
+                    className={`flex items-center justify-center px-5 py-2.5 lg:px-[10px] ${
                       index === 1
                         ? "border-x-2 border-solid border-[#E4E2D7]"
                         : ""
                     }`}
                   >
-                    <img className="p-2.5" src={info.src} alt={info.alt} />
+                    <img
+                      className="p-2.5 lg:pl-[2px]"
+                      src={info.src}
+                      alt={info.alt}
+                    />
                     <div>
-                      <p className="text-[#878680] text-[12px]">{info.text}</p>
-                      <p className="whitespace-nowrap text-[14px] font-bold">
+                      <p className="text-[#878680] text-[12px] lg:text-[10px]">
+                        {info.text}
+                      </p>
+                      <p className="whitespace-nowrap text-[14px] lg:text-[12px] font-bold">
                         {info.additonalText}
                       </p>
                     </div>
