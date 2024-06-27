f68e732003206b2009c8fff5e01068560307d1d5
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-27 22:57:18 +0400

    Add: News Section Responsive

--- src/pages/HomePage/NewsSection/NewsSection.tsx
+++ src/pages/HomePage/NewsSection/NewsSection.tsx
@@ -5,10 +5,10 @@ export function NewsSection() {
   return (
     <article className="flex flex-col items-center justify-center w-full max-w-[1240px] mb-[113px] px-[40px]">
       <h4 className="text-[24px] text-[#EEC044]">From the Blog</h4>
-      <h2 className="text-[48px] text-[#1F1E17] font-bold leading-[55px] mb-[50px]">
+      <h2 className="text-[48px] text-[#1F1E17] font-bold leading-[55px] mb-[50px] md:text-[36px]">
         News & Articles
       </h2>
-      <figure className="grid grid-cols-[1fr_1fr_1fr]">
+      <figure className="grid grid-cols-[1fr_1fr_1fr] lg:grid-cols-[1fr] lg:gap-y-[15px]">
         <img
           className="cursor-pointer w-full"
           src={foodProductionImage}
