a0270719aec5f78851b57a683c0294bca87d5838
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 19:19:05 +0400

    Add: What They Say Section HTML

--- src/pages/AboutPage/WhatTheySaySection/WhatTheySaySection.tsx
+++ src/pages/AboutPage/WhatTheySaySection/WhatTheySaySection.tsx
@@ -1,5 +1,34 @@
 export function WhatTheySaySection() {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <h5>Our Testimonials</h5>
+        <h2>What They Say</h2>
+        <div>
+          <figure>
+            <img src="" alt="Male Person Image" />
+            <figcaption>
+              There are many variations of passages available, but the majority
+              have suffered alterations in some form by injected humor or
+              randomness
+            </figcaption>
+            <h4>Bonnie Tolbet</h4>
+            <p>Customer</p>
+          </figure>
+          <figure>
+            <img src="" alt="Male Person Image" />
+            <figcaption>
+              There are many variations of passages available, but the majority
+              have suffered alterations in some form by injected humor or
+              randomness
+            </figcaption>
+            <h4>Bonnie Tolbet</h4>
+            <p>Customer</p>
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default WhatTheySaySection;
