0b90cdd66c7331d4d9a1b110ab05d66490ab36d2
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 14:18:00 +0400

    Add: Video Section On About Page style

--- src/pages/AboutPage/AboutVideo/AboutVideo.tsx
+++ src/pages/AboutPage/AboutVideo/AboutVideo.tsx
@@ -1,13 +1,32 @@
+import ecoFriendlyVideo from "src/assets/eco-friendly-video.mp4";
+
 export function AboutVideo() {
   return (
-    <section>
-      <h2>ECO-Friendly Products can be Made from Scratch</h2>
-      <video src="" loop autoPlay muted></video>
-      <div>
-        <p>Agriculture Products</p>
-        <p>Projects Completed</p>
-        <p>Satisfied Clients</p>
-        <p>Experts Farmers</p>
+    <section className="w-full max-w-[1920px] mb-[120px] relative">
+      <h2 className="text-[#ffffff] font-bold text-[60px] absolute left-[30%] bottom-2/4 text-center">
+        ECO-Friendly Products can
+        <br /> be Made from Scratch
+      </h2>
+      <video
+        className="w-full max-w-[1920px]"
+        src={ecoFriendlyVideo}
+        loop
+        autoPlay
+        muted
+      ></video>
+      <div className="bg-[#1F1E17] h-[184px] w-full grid grid-cols-[1fr_1fr_1fr_1fr]">
+        <p className="text-[#A5A49A] text-[18px] flex items-center justify-center">
+          Agriculture Products
+        </p>
+        <p className="text-[#A5A49A] text-[18px] flex items-center justify-center">
+          Projects Completed
+        </p>
+        <p className="text-[#A5A49A] text-[18px] flex items-center justify-center">
+          Satisfied Clients
+        </p>
+        <p className="text-[#A5A49A] text-[18px] flex items-center justify-center">
+          Experts Farmers
+        </p>
       </div>
     </section>
   );
