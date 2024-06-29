31558ce7126e526477fa52e316cc4bbc2322d572
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 13:53:29 +0400

    Add: About Video Section HTML

--- src/pages/AboutPage/AboutVideo/AboutVideo.tsx
+++ src/pages/AboutPage/AboutVideo/AboutVideo.tsx
@@ -1,5 +1,16 @@
 export function AboutVideo() {
-  return <div></div>;
+  return (
+    <section>
+      <h2>ECO-Friendly Products can be Made from Scratch</h2>
+      <video src="" loop autoPlay muted></video>
+      <div>
+        <p>Agriculture Products</p>
+        <p>Projects Completed</p>
+        <p>Satisfied Clients</p>
+        <p>Experts Farmers</p>
+      </div>
+    </section>
+  );
 }
 
 export default AboutVideo;
