4e85670e1f6766572115646323faeae762734be7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 23:15:32 +0400

    Add: Our Farmers Section Responsive

--- src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
+++ src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
@@ -4,14 +4,14 @@ import davidMartin from "src/assets/david-martin.svg";
 
 export function OurFarmersSection() {
   return (
-    <section className="w-full max-w-[1200px] mb-[120px] flex items-center justify-center flex-col">
+    <section className="w-full max-w-[1200px] mb-[120px] flex items-center justify-center flex-col px-[40px]">
       <h5 className="text-[#EEC044] text-[24px] flex justify-center">
         Team Members
       </h5>
       <h2 className="flex justify-center text-[48px] mb-[50px] text-[#1F1E17] leading-[50px] font-bold sm:text-[36px]">
         Meet Our Farmers
       </h2>
-      <figure className="grid grid-cols-[1fr_1fr_1fr] gap-x-[30px]">
+      <figure className="grid grid-cols-[1fr_1fr_1fr] gap-x-[30px] md:grid-cols-[1fr] md:gap-y-[20px]">
         <img className="w-full" src={kevinSmith} alt="Kevin Smith's farm" />
         <img className="w-full" src={jessicaBrown} alt="Jessica Brown's farm" />
         <img className="w-full" src={davidMartin} alt="David Martin's farm" />
