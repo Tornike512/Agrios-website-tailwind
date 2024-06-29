a092b5bd621dc5a732a0886a476f03a8ce6f4b4f
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 21:57:57 +0400

    Add: Our Farmers Section HTML

--- src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
+++ src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
@@ -1,5 +1,19 @@
+import kevinSmith from "src/assets/kevin-smith.svg";
+import jessicaBrown from "src/assets/jessica-brown.svg";
+import davidMartin from "src/assets/david-martin.svg";
+
 export function OurFarmersSection() {
-  return <section></section>;
+  return (
+    <section>
+      <h5>Team Members</h5>
+      <h2>Meet Our Farmers</h2>
+      <figure>
+        <img src={kevinSmith} alt="Kevin Smith's farm" />
+        <img src={jessicaBrown} alt="Jessica Brown's farm" />
+        <img src={davidMartin} alt="David Martin's farm" />
+      </figure>
+    </section>
+  );
 }
 
 export default OurFarmersSection;
