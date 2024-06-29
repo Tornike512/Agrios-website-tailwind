8941ae0ca2dc4f542d42886029f05ef5984af018
Tornike Tsagareishvili <dht5152@gmail.com>
2024-06-29 22:03:03 +0400

    Add: Our Farmers Section Style

--- src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
+++ src/pages/AboutPage/OurFarmersSection/OurFarmersSection.tsx
@@ -4,13 +4,17 @@ import davidMartin from "src/assets/david-martin.svg";
 
 export function OurFarmersSection() {
   return (
-    <section>
-      <h5>Team Members</h5>
-      <h2>Meet Our Farmers</h2>
-      <figure>
-        <img src={kevinSmith} alt="Kevin Smith's farm" />
-        <img src={jessicaBrown} alt="Jessica Brown's farm" />
-        <img src={davidMartin} alt="David Martin's farm" />
+    <section className="w-full max-w-[1200px] mb-[120px] flex items-center justify-center flex-col">
+      <h5 className="text-[#EEC044] text-[24px] flex justify-center">
+        Team Members
+      </h5>
+      <h2 className="flex justify-center text-[48px] mb-[50px] text-[#1F1E17] leading-[50px] font-bold sm:text-[36px]">
+        Meet Our Farmers
+      </h2>
+      <figure className="grid grid-cols-[1fr_1fr_1fr] gap-x-[30px]">
+        <img className="w-full" src={kevinSmith} alt="Kevin Smith's farm" />
+        <img className="w-full" src={jessicaBrown} alt="Jessica Brown's farm" />
+        <img className="w-full" src={davidMartin} alt="David Martin's farm" />
       </figure>
     </section>
   );
