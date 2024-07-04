b4bbd8ce481fb39aa6645ba6a7cbc4e059f44678
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-04 21:05:45 +0400

    Add: Projects Page Resposnive

--- src/pages/ProjectsPage/ProjectHeader/ProjectHeader.tsx
+++ src/pages/ProjectsPage/ProjectHeader/ProjectHeader.tsx
@@ -3,7 +3,11 @@ import projectHeader from "src/assets/project-header.svg";
 export function ProjectHeader() {
   return (
     <figure className="mb-[120px]">
-      <img className="w-full" src={projectHeader} alt="Project Header Image" />
+      <img
+        className="w-full lg:h-[400px] lg:object-cover"
+        src={projectHeader}
+        alt="Project Header Image"
+      />
     </figure>
   );
 }
