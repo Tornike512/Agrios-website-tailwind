5abe796b5c4e5f3a56a6db0190d90c1f474e6ee7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-06 00:52:00 +0400

    Add: Projects Navigation Style

--- src/pages/ProjectsPage/ProjectsNav/ProjectsNav.tsx
+++ src/pages/ProjectsPage/ProjectsNav/ProjectsNav.tsx
@@ -7,28 +7,49 @@ export function ProjectsNav() {
     <>
       {projects.projects.map((project) => {
         return (
-          <section key={project.id}>
-            <img src={project.image} alt="Main Image" />
-            <div>
-              <h2>{project.title}</h2>
-              <p>{project.firstDescription}</p>
-              <strong>{project.secondaryTitle}</strong>
-              <p>{project.secondaryDescription}</p>
-              <p>{project.tertiaryDescription}</p>
-              <h3>Challenges</h3>
-              <strong>{project.challenges}</strong>
-              <p>{project.footerDescription}</p>
+          <section
+            className="mb-[120px] w-full px-[40px] max-w-[1200px]"
+            key={project.id}
+          >
+            <img className="mb-[40px]" src={project.image} alt="Main Image" />
+            <div className="flex">
+              <article className="w-full mr-[30px]">
+                <h2 className="text-[30px] font-bold mb-[8px]">
+                  {project.title}
+                </h2>
+                <p className="text-[#878680] text-[16px] mb-[33px]">
+                  {project.firstDescription}
+                </p>
+                <p className="text-[16px] font-bold mb-[33px]">
+                  {project.secondaryTitle}
+                </p>
+                <p className="text-[#878680] text-[16px] mb-[33px]">
+                  {project.secondaryDescription}
+                </p>
+                <p className="text-[#878680] text-[16px] mb-[33px]">
+                  {project.tertiaryDescription}
+                </p>
+                <h3 className="text-[33px] font-bold mb-[33px]">Challenges</h3>
+                <p className="text-[16px] font-bold mb-[33px] w-[55%] ml-[30px]">
+                  {project.challenges}
+                </p>
+                <p className="text-[#878680] text-[16px] mb-[33px]">
+                  {project.footerDescription}
+                </p>
+              </article>
+              <aside className="rounded-[10px] p-[60px] border-t-[#EEC044] border-[5px] w-[40%] shadow-[0px_0px_20px_#e3e3e3] h-fit">
+                <p className="text-[14px] text-[#878680]">Services:</p>
+                <p className="font-bold text-[16px] mb-[26px]">
+                  {project.title}
+                </p>
+                <p className="text-[14px] text-[#878680]">Farmer:</p>
+                <p className="font-bold text-[16px] mb-[26px]">Mike Hardson</p>
+                <p className="text-[14px] text-[#878680]">Duration:</p>
+                <p className="font-bold text-[16px] mb-[26px]">4.5 Months</p>
+                <p className="text-[14px] text-[#878680]">Location:</p>
+                <p className="font-bold text-[16px]">Broklyn, New Your</p>
+              </aside>
             </div>
-            <aside>
-              <p></p>
-              <strong></strong>
-              <p></p>
-              <strong></strong>
-              <p></p>
-              <strong></strong>
-              <p></p>
-              <strong></strong>
-            </aside>
           </section>
         );
       })}
