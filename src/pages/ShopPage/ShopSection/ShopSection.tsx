58bbd7cc150994b5b055c508e9e0862380013982
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-06 21:15:13 +0400

    Add: Stop Section Style

--- src/pages/ShopPage/ShopSection/ShopSection.tsx
+++ src/pages/ShopPage/ShopSection/ShopSection.tsx
@@ -1,74 +1,102 @@
 export function ShopSection() {
   return (
-    <section className="mb-[120px]">
-      <div>
-        <section>
-          <p>Showing 1–9 of 10 results</p>
-          <select>
+    <section className="w-full max-w-[1200px] mb-[120px] flex px-[40px]">
+      <form className="mr-[30px]">
+        <input
+          className="bg-[#EEC044] mb-[30px] text-[18px] text-[#ffffff] font-bold placeholder-[#F3F3F3] p-[30px] rounded-[10px]"
+          placeholder="Search products."
+        />
+        <div className="p-[30px] border-[2px] border-solid border-[#8E8E8E] rounded-[10px]">
+          <label className="text-[20px] font-bold">Price</label>
+          <input
+            className="w-full h-[5px] appearance-none bg-[#EEC044] mb-[10px]"
+            type="range"
+          />
+          <span className="flex items-center justify-between">
+            <p className="text-[#878680] text-[12px]">$20 - $100</p>
+            <button className="text-[14px] text-[#ffffff] font-bold rounded-[10px] py-[10px] px-[15px] bg-[#4BAF47] hover:bg-[#54c950]">
+              Apply
+            </button>
+          </span>
+        </div>
+      </form>
+      <div className="w-full">
+        <section className="flex items-center justify-between mb-[50px]">
+          <p className="text-[#878680] text-[18px]">
+            Showing 1–9 of 10 results
+          </p>
+          <select className="border p-[30px] rounded-[10px] border-solid border-[#cecece] text-[#878680]">
             <option value="Default sorting">Default sorting</option>
             <option value="Price (High to Low)">Price (High to Low)</option>
             <option value="Price (Low to High)">Price (Low to High)</option>
           </select>
         </section>
-        <section>
+        <section className="grid grid-cols-[1fr_1fr_1fr] gap-x-[30px] gap-y-[30px]">
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
           <div>
-            <img src="" alt="Plant Image" />
-            <h3>Apple</h3>
-            <p>$50.00</p>
+            <figure className="w-[270px] h-[270px] bg-[#EFEFEF] rounded-[10px]">
+              <img src="" alt="Plant Image" />
+            </figure>
+            <h3 className="font-bold text-[22px]">Apple</h3>
+            <p className="text-[16px] text-[#4BAF47] font-bold">$50.00</p>
           </div>
         </section>
       </div>
-      <form>
-        <input type="text" placeholder="Search products." />
-        <div>
-          <strong>Price</strong>
-          <input type="range" />
-          <span>
-            <p>$20 - $100</p>
-            <button>Apply</button>
-          </span>
-        </div>
-      </form>
     </section>
   );
 }
