72b8a50ad385b3dedf6c32437109712163e0b474
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-18 16:47:27 +0400

    Add: Contacts Map Section HTML

---
+++ src/pages/ContactPage/ContactMap/ContactMap.tsx
@@ -0,0 +1,19 @@
+export function ContactMap() {
+  return (
+    <section>
+      <img src="" alt="" />
+      <form>
+        <h5>Contact us</h5>
+        <h2>Write a Message</h2>
+        <div>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email Address" />
+        </div>
+        <input type="text" placeholder="Write a Message" />
+        <button>Send a Message</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactMap;
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,11 +1,13 @@
 import { ContactGrid } from "./ContactGrid";
 import { ContactHeader } from "./ContactHeader";
+import { ContactMap } from "./ContactMap";
 
 export function ContactPage() {
   return (
     <section className="w-full flex flex-col justify-center items-center">
       <ContactHeader />
       <ContactGrid />
+      <ContactMap />
     </section>
   );
 }
