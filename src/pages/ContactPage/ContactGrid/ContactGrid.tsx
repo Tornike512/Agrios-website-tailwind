8a4afda9d69a5a920342b6f402fcaf40e737ea8b
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-18 16:40:15 +0400

    Add: Contacts Grid Section Style

--- src/pages/ContactPage/ContactGrid/ContactGrid.tsx
+++ src/pages/ContactPage/ContactGrid/ContactGrid.tsx
@@ -1,22 +1,28 @@
 export function ContactGrid() {
   return (
-    <section>
-      <div>
-        <h3>About</h3>
-        <p>
-          Lorem ipsum is simply free text used by copytypinh refreshing. Neque
-          porro est qui
+    <section className="mb-[120px] px-[40px] w-full max-w-[1200px] grid grid-cols-[1fr_1fr_1fr] gap-x-[30px] text-[#ffffff]">
+      <div className="rounded-[10px] px-[50px] py-[40px] bg-[#4BAF47]">
+        <h3 className="mb-[10px] font-bold text-[30px]">About</h3>
+        <p className="leading-[30px]">
+          Lorem ipsum is simply free text used by copytyping refreshing. Neque
+          porro est qui.
         </p>
       </div>
-      <div>
-        <h3>Contact</h3>
-        <p>
-          +1- (246) 333-0079 support@agrios.com Mon - Fri: 7:00 am - 6:00 pm
+      <div className="rounded-[10px] px-[50px] py-[40px] bg-[#C5CE38]">
+        <h3 className="mb-[10px] font-bold text-[30px]">Contact</h3>
+        <p className="leading-[30px]">
+          +1- (246) 333-0079
+          <br />
+          support@agrios.com
+          <br />
+          Mon - Fri: 7:00 am - 6:00 pm
         </p>
       </div>
-      <div>
-        <h3>Address</h3>
-        <p>66 Broklun Road Golden Street, New Your United States of Ameriaca</p>
+      <div className="rounded-[10px] px-[50px] py-[40px] bg-[#EEC044]">
+        <h3 className="mb-[10px] font-bold text-[30px]">Address</h3>
+        <p className="leading-[30px]">
+          66 Brooklyn Road, Golden Street, New York, United States of America
+        </p>
       </div>
     </section>
   );
