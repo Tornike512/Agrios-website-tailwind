06616413a7f6a8f388c403d8293de773a90bb821
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-20 14:32:33 +0400

    Add: Contact Section Header Responsive

--- src/pages/ContactPage/ContactHeader/ContactHeader.tsx
+++ src/pages/ContactPage/ContactHeader/ContactHeader.tsx
@@ -4,7 +4,7 @@ export function ContactHeader() {
   return (
     <figure className="mb-[120px]">
       <img
-        className="w-full lg:[400px] object-cover"
+        className="w-full lg:[400px] object-cover lg:h-[400px] lg:object-cover"
         src={contactHeader}
         alt="Contact Header Image"
       />
